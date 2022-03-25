9e7ad5e79d9d314f151370c8152218ad9bd1bd16
ryotsuf <mail.ryoto.ino@gmail.com>
2022-03-25 12:18:10 +0900

    deviseの実装、ユーザー管理機能途中まで

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password, presence: true
+  validates :birthday, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
 end
+
+
+# devise側のバリデーションによってemailは空白ダメ、一意性、@を含むという制限が設けられている
+# パスワード側の制限は空白ダメ、6文字以上128文字以下であること
