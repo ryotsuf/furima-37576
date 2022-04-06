33eb79fc9d2f4fee3e62f8331732181ee42043c1
ryotsuf <mail.ryoto.ino@gmail.com>
2022-04-06 16:32:40 +0900

    モデルとテーブルを作成、画像投稿機能とバリデーションの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,5 +6,11 @@ class ItemsController < ApplicationController
 #   @user = User.new
 #  end
 
+def items_params
+  params.require(:item).permit(:image, :name, :explanation,
+    :category_id, :condition_id , :delivery_fee_id,
+    :region_delivery_id, :days_to_deliver_id, :price, 
+  ).merge(user_id: current_user.id)
+end
 
 end
---
+++ app/models/item.rb
@@ -0,0 +1,31 @@
+class Item < ApplicationRecord
+
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_fee
+  belongs_to :region_delivery
+  belongs_to :days_to_deliver
+  has_one_attached :image
+
+  
+  with_options presence: true do
+
+    validates :name
+    validates :explanation
+    validates :image
+
+    with_options numericality: { other_than: 1 , message: "can't be blank" } do
+      validates :category_id
+      validates :condition_id
+      validates :delivery_fee_id
+      validates :region_delivery_id
+      validates :days_to_deliver_id
+    end
+    
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}
+  end
+
+end
