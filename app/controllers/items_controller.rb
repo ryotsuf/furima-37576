669a9bab92a0a6338b6dada5953d1a74ebb0cb8c
ryotsuf <mail.ryoto.ino@gmail.com>
2022-04-12 15:49:44 +0900

    共通する記述をbefore actionにセット

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :find_item, only: [:show, :edit, :update]
 
  def index
   @items = Item.order("created_at DESC")
@@ -19,18 +20,18 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    
   end
 
   def edit
-    @item = Item.find(params[:id])
+    
     unless current_user.id == @item.user_id
       redirect_to root_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
+    
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -45,6 +46,10 @@ class ItemsController < ApplicationController
       :category_id, :condition_id , :delivery_fee_id,
       :region_delivery_id, :days_to_deliver_id, :price, 
     ).merge(user_id: current_user.id)
+  end
+
+  def find_item
+    @item = Item.find(params[:id])
   end
 
 end
