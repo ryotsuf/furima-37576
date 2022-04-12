c0c2e72ad9cbd1f524b8242330bab5a55962d0d0
ryotsuf <mail.ryoto.ino@gmail.com>
2022-04-12 17:44:00 +0900

    レビュー箇所の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -40,10 +40,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-
     item = Item.find(params[:id])
-    if item.destroy
-      redirect_to root_path
+    if item.user_id == current_user.id
+        if item.destroy
+          redirect_to root_path
+        end
     end
   end
 
